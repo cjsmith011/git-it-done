2073e110554e5358b22647a50f0e5b08a5e38929
Smith <carliesmith@amrock.com>
2022-01-06 15:34:17 -0500

    adding warning at bottom for >30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,7 +10,10 @@ var getRepoIssues = function(repo) {
          response.json().then(function(data) {
              //pass response data to dom
              displayIssues(data);
-
+            //check if api has paginated issues (to look for 30)
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
        });
     
      } else {
@@ -48,6 +51,19 @@ var displayIssues = function(issues) {
         }
     };
 }
+
+var displayWarning = function(repo) {
+    //add text to a container to warn user of a limit of 30 items
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 }
 
 
